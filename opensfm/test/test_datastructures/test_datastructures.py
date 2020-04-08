6d978adb0d8195fb17812d966f3319562a776a65
Fabian Schenk <fabianschenk@mapillary.com>
2020-04-08 17:51:21 +0200

    test: restructure to be a valid unit test

--- opensfm/test/test_datastructures/test_datastructures.py
+++ opensfm/test/test_datastructures/test_datastructures.py
@@ -2,7 +2,167 @@ import numpy as np
 from opensfm import pymap
 
 
+def test_shot_cameras():
+    """Test that shot cameras are created and deleted correctly"""
+    map_mgn = pymap.Map()
+    cam_model = pymap.Camera()
+    n_cams = 10
+    cams = []
+    for i in range(0, n_cams):
+        cam = map_mgn.create_shot_camera(i, cam_model, "cam" + str(i))
+        cams.append(cam)
+    assert len(cams) == map_mgn.number_of_cameras()
 
+    # Test double create
+    for i in range(0, n_cams):
+        cam = map_mgn.create_shot_camera(i, cam_model, "cam" + str(i))
+        assert cams[i] == cam
+
+    # Delete 2 out of the ten cameras
+    map_mgn.remove_shot_camera(0)
+    assert n_cams - 1 == map_mgn.number_of_cameras()
+    map_mgn.remove_shot_camera(3)
+    assert n_cams - 2 == map_mgn.number_of_cameras()
+
+    # Double deletion
+    map_mgn.remove_shot_camera(3)
+    assert n_cams - 2 == map_mgn.number_of_cameras()
+    shots = map_mgn.get_all_cameras()
+    shot_idx = sorted([shot_id for shot_id in shots.keys()])
+    remaining_shots = [1, 2, 4, 5, 6, 7, 8, 9]
+    # shot_idx.sort()
+    assert shot_idx == remaining_shots
+    # remove the rest
+    for shot_id in remaining_shots:
+        map_mgn.remove_shot_camera(shot_id)
+    assert map_mgn.number_of_shots() == 0
+
+
+def test_pose():
+    pose = pymap.Pose()
+    # Test default
+    assert np.allclose(pose.get_cam_to_world(), np.eye(4), 1e-10)
+    assert np.allclose(pose.get_world_to_cam(), np.eye(4), 1e-10)
+
+    # Test with other matrix
+    T = np.array([[1, 2, 2, 30],
+                  [3, 1, 3, 20],
+                  [4, 3, 1, 10],
+                  [8, 7, 5, 1]], dtype=np.float)
+
+    # TODO: Set with actual transformation matrix!
+    pose.set_from_world_to_cam(T)
+    assert np.allclose(pose.get_world_to_cam(), T)
+    assert np.allclose(pose.get_cam_to_world(), np.linalg.inv(T))
+
+    pose.set_from_cam_to_world(T)
+    assert np.allclose(pose.get_cam_to_world(), T)
+    assert np.allclose(pose.get_world_to_cam(), np.linalg.inv(T)) 
+
+
+def test_shots():
+    shots = []
+    map_mgn = pymap.Map()
+    cam_model = pymap.Camera()
+    cam = map_mgn.create_shot_camera(0, cam_model, "cam" + str(0))
+    pose = pymap.Pose()
+    n_shots = 10
+    for shot_id in range(n_shots):
+        # Test next_id function
+        assert shot_id == map_mgn.next_unique_shot_id()
+        shot = map_mgn.create_shot(shot_id, cam, "shot" + str(shot_id), pose)
+        shots.append(shot)
+
+    # Create again and test if the already created pointer is returned
+    for shot_id in range(n_shots):
+        shot = map_mgn.create_shot(shot_id, cam, "shot" + str(shot_id), pose)
+        assert shot == shots[shot_id]
+    assert map_mgn.number_of_shots() == n_shots
+    # Test default parameters
+    shots.append(map_mgn.create_shot(n_shots, cam, "shot" + str(shot_id)))
+    shots.append(map_mgn.create_shot(n_shots + 1, cam))
+    assert map_mgn.number_of_shots() == n_shots + 2
+    delete_ids = [0, 1, 2, 3, 7, 7, 7, 7]
+    remaining_ids = [4, 5, 6, 8, 9, 10, 11]
+    n_deleted = len(np.unique(delete_ids))
+    for shot_id in delete_ids:
+        map_mgn.remove_shot(shot_id)
+
+    assert map_mgn.number_of_shots() == n_shots + 2 - n_deleted
+    shot_ids = sorted([shot_id for shot_id in map_mgn.get_all_shots()])
+    assert remaining_ids == shot_ids
+
+    # Try to create only the deleted ones again
+    for shot_id in range(n_shots):
+        shot = map_mgn.create_shot(shot_id, cam, "shot" + str(shot_id), pose)
+    assert map_mgn.number_of_shots() == n_shots + 2
+    shot_ids = sorted([shot_id for shot_id in map_mgn.get_all_shots()])
+    all_ids = [idx for idx in range(n_shots + 2)]
+    assert shot_ids == all_ids
+
+    # Delete all shots
+    shot_ids = sorted([shot_id for shot_id in map_mgn.get_all_shots()])
+    for shot_id in shot_ids:
+        map_mgn.remove_shot(shot_id)
+    assert map_mgn.number_of_shots() == 0
+
+    assert map_mgn.next_unique_shot_id() == n_shots + 2
+    
+    map_mgn.create_shot(0, cam, "shot" + str(shot_id), pose)
+    assert map_mgn.next_unique_shot_id() == n_shots + 2
+    map_mgn.create_shot(1, cam, "shot" + str(shot_id), pose)
+    assert map_mgn.next_unique_shot_id() == n_shots + 2
+    map_mgn.create_shot(1000, cam, "shot" + str(shot_id), pose)
+    assert map_mgn.next_unique_shot_id() == 1001
+
+
+def test_landmarks():
+    map_mgn = pymap.Map()
+    n_lms = 100
+    lms = []
+    for lm_id in range(n_lms):
+        pos = np.random.rand(3)
+        assert map_mgn.next_unique_landmark_id() == lm_id
+        lm = map_mgn.create_landmark(lm_id, pos, "lm" + str(lm_id))
+        lms.append(lm)
+        assert np.allclose(pos.flatten(), lm.get_global_pos())
+    assert n_lms == map_mgn.number_of_landmarks()
+    # Test double creation
+    for lm_id in range(n_lms):
+        pos = np.random.rand(3)
+        lm = map_mgn.create_landmark(lm_id, pos, "lm" + str(lm_id))
+        assert lm == lms[lm_id]
+    assert n_lms == map_mgn.number_of_landmarks()
+    # Test pos update
+    for lm_id in range(n_lms):
+        pos = np.random.rand(3)
+        lm = lms[lm_id]
+        lm.set_global_pos(pos)
+        assert np.allclose(lm.get_global_pos(), pos)
+
+    # Remove every second landmark
+    remaining_ids = [idx for idx in range(1, 100, 2)]
+    for lm_id in range(0, 100, 2):
+        map_mgn.remove_landmark(lm_id)
+    lm_ids = sorted([lm_id for lm_id in map_mgn.get_all_landmarks()])
+    # check if remaining lms in the map are equal to the expected
+    assert len(lm_ids) == len(remaining_ids)
+    assert lm_ids == remaining_ids
+    assert n_lms - len(remaining_ids) == map_mgn.number_of_landmarks()
+
+    map_mgn.create_landmark(1000, pos, "lm" + str(lm_id))
+    assert map_mgn.next_unique_landmark_id() == 1001
+    assert len(remaining_ids) + 1 == map_mgn.number_of_landmarks()
+
+
+test_landmarks()
+test_shot_cameras()
+test_pose()
+test_shots()
+
+
+
+"""
 def test_create_shot_cameras(n_cams, map_mgn, cam_model):
   cams = []
   for i in range(0, n_cams):
@@ -163,13 +323,6 @@ def test_larger_problem(map_mgn):
   for shot in shots:
       print("{} observations in shot {}".format(shot.compute_num_valid_pts(), shot.id_))
 
-  # for shot in shots: 
-    # feat_obs = np.asarray(np.random.rand(50)*1000%200, dtype=np.int)
-    # print("Removing from {} with a tot of {} observations:".format(shot.id_, shot.compute_num_valid_pts()))
-    # for feat_id in feat_obs:
-      # map_mgn.remove_observation(shot, feat_id)
-    # print("Removed from {} with a tot of {} observations:".format(shot.id_, shot.compute_num_valid_pts()))
-
   # Just try to delete it from all the shots
   # To test what happens if we delete it from a shot without the observation
 
@@ -208,4 +361,4 @@ test_remove_landmarks(map_mgn)
 #Now create a large problem
 test_larger_problem(map_mgn)
 
-
+"""
